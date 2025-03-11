c017b85ecb285bbd46a126bb30f15174f19413be
joseph <jsakr212005@gmail.com>
2025-03-11 15:21:31 +0200

    js work
    tech wok

---
+++ javascript.js
@@ -0,0 +1,19 @@
+const item= [
+    { Image:"./pitcures/apple watch.png " ,    title:"appel watch" , quantity:"10" , price:  "500$"},
+    { Image:"./pitcures/electronicvape.png" ,  title:"Vape  Electronics" , quantity:"20" , price:  "20$"},
+    { Image:"./pitcures/samsong01.png" ,       title:"B12 Pro" , quantity:"5" , price:  "10$"},
+    { Image:"./pitcures/lenovolaptop.png" ,    title:"Lenovo Laptop" , quantity:"5" , price:  "250$"},
+    { Image:"./pitcures/vapeb1.png" ,          title:"ELf Bar" , quantity:"10" , price:  "15$"},
+    { Image:"./pitcures/electronicvape1.png" , title:"VooPoo" , quantity:"17" , price:  "12$"},
+    { Image:"./pitcures/lenovo.png" ,          title:"Lenovo Watch" , quantity:"4" , price:  "45$"},
+    { Image:"./pitcures/vaporesso.png" ,    title:"Vaporesso" , quantity:"5" , price:  "10$"},
+    { Image:"../pitcures/samsung.png" ,    title:"Cheap Watch" , quantity:"6" , price:  "30$"},
+    { Image:"./pitcures/Skyhunt.png" ,    title:"Sky hunt" , quantity:"4" , price:  "14$"},
+    { Image:"./pitcures/desktop.png" ,    title:"Pc Gaming" , quantity:"5" , price:  "1000$"},
+    { Image:"./pitcures/screen appel.png" ,    title:"Screen Appel" , quantity:"3" , price:  "300$"},
+    { Image:"./pitcures/gaming chair.png" ,    title:"Gaming Chair" , quantity:"4" , price:  "200$"},
+    { Image:"./pitcures/gaming headphones.png" ,    title:"Head phones Gaming" , quantity:"4" , price:  "30$"},
+    
+
+]
+
